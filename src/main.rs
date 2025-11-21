0ca683a81b08a282910466647f7c77848723c18c
ouiyamcode <williaminiziato54@gmail.com>
2025-11-21 12:05:45 +0100

    et pourquoi pas?

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ fn main() {
     EspLogger::initialize_default();
     log::info!("Booting, initializing USB host");
 
+    let mut client_handle: usb_host_client_handle_t = core::ptr::null_mut();
+
     //Native ESP-IDF needs to be unsafe, because we are calling raw C functions
     unsafe{ 
         let cfg = usb_host_config_t{         //Minimal configuration, C structure to configure driver USB Host
@@ -30,8 +32,6 @@ fn main() {
             ..core::mem::zeroed()
         };
 
-        let mut client_handle: usb_host_client_handle_t = core::ptr::null_mut();
-
         let err = usb_host_client_register(&client_cfg, &mut client_handle);
         if err != ESP_OK{
             log::error!("usb_host_client_register failed: {}", err);
@@ -41,8 +41,32 @@ fn main() {
         }
     }
 
-    loop{ //loop to test if the firmware works
-        log::info!("Main loop alive (USB host ready, MSC not yet configured)...");
-        thread::sleep(Duration::from_secs(5));
+    loop{ //loop: poll for USB events
+        //log::info!("Main loop alive (USB host ready, MSC not yet configured)..."); //these two lines to test if the firmware works 
+        //thread::sleep(Duration::from_secs(5));
+
+        unsafe{
+            //let mut msg: usb_host_client_event_msg_t = core::mem::zeroed();
+
+            let ret = usb_host_client_handle_events(client_handle, 10);
+
+            match ret {
+                ESP_OK => {
+                    log::info!("USB event handled");
+                }
+                ESP_ERR_TIMEOUT => {
+                    //no events during the timeout => normal
+                }
+                ESP_ERR_INVALID_STATE => {
+                    log::error!("USB Host Library not installed");
+                }
+                ESP_ERR_INVALID_ARG => {
+                    log::error!("Invalid usb_host_client_handle_events() argument");
+                }
+                _ => {
+                    log::warn!("Unknown error from usb_host_client_handle_events(): {}", ret);
+                }
+            }
+        }
     }
 }
